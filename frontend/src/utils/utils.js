8624fb5c8a57433926eef077120e9f8adbbe215a
Jake Samuels <samuels242@googlemail.com>
2025-05-31 14:38:48 +0100

    Create task util function

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -6,12 +6,29 @@ const BASE_URL = "http://localhost:8000/api/tasks";
 export const fetchAllTasks = async () => {
   try {
     const response = await axios.get(BASE_URL);
-    const data = response.data;
+    const data = response.data.tasks;
 
-    console.log(data);
     return data;
   } catch (err) {
     console.error("Error fetching tasks:", err);
+    throw err;
+  }
+};
+
+// Create new task
+export const createTask = async (formData) => {
+  try {
+    const response = await axios({
+      method: "POST",
+      data: formData,
+      url: BASE_URL,
+    });
+
+    const newTaskFromServer = await response.data.task;
+
+    return newTaskFromServer;
+  } catch (err) {
+    console.error("Error creating task:", err);
     throw err;
   }
 };
