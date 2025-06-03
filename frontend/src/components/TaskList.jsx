c4d6db3226a9dec947fcc815da0c7c0580d193ef
Jake Samuels <samuels242@googlemail.com>
2025-06-03 18:03:21 +0100

    Add placeholder for empty task list

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,6 +1,19 @@
 import TaskItem from "./TaskItem";
 
 const TaskList = ({ tasks, modalOpen, setModalOpen, onTaskDeleted }) => {
+  if (tasks.length === 0)
+    return (
+      <p
+        style={{
+          textAlign: "center",
+          fontSize: "1.25rem",
+          fontWeight: "600",
+        }}
+      >
+        Start adding tasks
+      </p>
+    );
+
   return (
     <ul className="task-list">
       {tasks.map((task) => (
